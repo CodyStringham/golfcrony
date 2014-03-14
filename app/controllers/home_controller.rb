1e7f97dd4fa1289a5d8e2f545cccda2fd2b6f4d4
Andy Hayes <andythemanhayes@hotmail.com>
2014-03-14 16:31:50 -0600

    more weathers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+  	@ip_info = Geolocate.find
   end
   
   def account
--- lib/geolocate.rb
+++ lib/geolocate.rb
@@ -3,9 +3,10 @@ require 'uri'
 
 class Geolocate
 
-	def self.find(ip_address)
-		uri = URI("http://ipinfo.io/#{ip_address}/json")
-		Net::HTTP.get(uri)
+	def self.find(ip_address = nil)
+		uri = URI("http://ipinfo.io/#{ip_address + '/' if ip_address.present? }json")
+		response = Net::HTTP.get(uri)
+		JSON.parse(response)
 	end
 
 
